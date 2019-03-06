3d6b94da0e67b99e7f019275e72a5a8af76865d4
Laura <laura.jinks@gmail.com>
2019-03-06 08:40:38 -0600

    complete problems

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,38 @@
 <template>
   <div id="app">
-    <div id="exercise">
-   <!-- 1) Fill the <p> below with your Name and Age - using Interpolation -->
-    <p>VueJS is pretty cool - YOUR_NAME (AGE)</p>
+    <!-- 1) Fill the <p> below with your Name and Age - using Interpolation -->
+    <p>{{ name }}</p>
     <!-- 2) Output your age, multiplied by 3 -->
-    <p></p>
+    <p>age: {{ age * 3 }}</p>
     <!-- 3) Call a function to output a random float between 0 and 1 (Math.random()) -->
-    <p></p>
+    <p>random: {{ Math.random() }}</p>
     <!-- 4) Search any image on Google and output it here by binding the "src" attribute -->
     <div>
-        <img style="width:100px;height:100px">
+      <img v-bind:src="url" style="width:100px;height:100px">
     </div>
     <!-- 5) Pre-Populate this input with your name (set the "value" attribute) -->
     <div>
-        <input type="text">
+      <input v-bind:value="name" type="text">
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  data: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      name: "Laura",
+      age: 28,
+      url: "http://placekitten.com/g/200/200"
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
